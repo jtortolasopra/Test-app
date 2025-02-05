3c8ff6fd3019fc457c61f2d8615c44776505803e
TORTOLA LAHIGUERA Julian <julian.tortola2@soprasteria.com>
2025-02-05 17:21:59 +0100

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://test-app-3fim.onrender.com/',
   plugins: [
     vue(),
     vueJsx(),
